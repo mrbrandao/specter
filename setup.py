e2b603697d7fa98a931a98afeee3715cfb534797
Igor Brandao <mrbrandao@proton.me>
2023-10-14 18:40:56 -0300

    Adjusting autosetuptools package dependencies

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ setup(
   name='specter',
   version='0.1.0',
   include_package_data=True,
-  install_requires=['click'],
+  install_requires=['click>=8.1.7,<=9.0.0',
+                    'requests>=2.31.0,<=3.0.0'],
   package_dir={'':'src'},
-  #packages=find_packages(),
   packages=['specter'],
   py_modules=['specter'],
   entry_points={
