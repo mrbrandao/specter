83a1cbce160d60fa078cca3124ab6659be9f3790
Igor Brandao <mrbrandao@proton.me>
2023-10-14 18:03:42 -0300

    Converting parameters from argparse to click

--- src/specter/cli.py
+++ src/specter/cli.py
@@ -1,6 +1,7 @@
 """
 The main cli execution
 """
+from typing import Optional
 import click
 from . import __version__
 
@@ -14,17 +15,27 @@ def ver():
 @click.option('-v','--version', is_flag=True, help="Show version")
 @click.pass_context
 def cli(ctx, version):
+    """
+    cli is the main execution function
+    """
     ctx.ensure_object(dict)
     if version:
         ver()
 
 @cli.command()
-def stop():
-    click.echo("Stop the execution")
-
-@cli.command()
-def hello():
-    click.echo("Hello World Click!")
-
-if __name__ == '__main__':
-    cli(obj={})
+@click.option('-i','--input','input_',
+              default='/tmp/test', help='Path to input file',
+              show_default=True
+              )
+@click.option('-b','--branch', default='f38',
+              show_default=True,
+              help='Define the mdapi branch to search'
+              )
+@click.option('-l','--list','list_', is_flag=True,help='List dependencies')
+@click.pass_context
+def search(_ctx: click.Context, **kwargs):
+    """
+    Search for package dependencies
+    """
+    click.echo(f'searching {kwargs["input_"]}')
+    click.echo(f'searching {kwargs}')
